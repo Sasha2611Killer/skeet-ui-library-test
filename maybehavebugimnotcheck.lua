cf2ea6199a6c2bcd14740fd8a2e3baff789855b4
Sasha2611Killer <158747806+Sasha2611Killer@users.noreply.github.com>
2026-07-03 23:57:38 +0800

    Update maybehavebugimnotcheck.lua

--- maybehavebugimnotcheck.lua
+++ maybehavebugimnotcheck.lua
@@ -2796,9 +2796,9 @@ end
 
 							-- Проверяем клик по палитре
 							local onPalette = Mouse.X >= Content_Open_Holder.AbsolutePosition.X and 
-								Mouse.Y >= (Content_Open_Holder.AbsolutePosition.Y + 36 + (36/2)) and 
+								Mouse.Y >= (Content_Open_Holder.AbsolutePosition.Y and 
 								Mouse.X <= Content_Open_Holder.AbsolutePosition.X + Content_Open_Holder.AbsoluteSize.X and 
-								Mouse.Y <= Content_Open_Holder.AbsolutePosition.Y + Content_Open_Holder.AbsoluteSize.Y + 36 + (36/2)
+								Mouse.Y <= Content_Open_Holder.AbsolutePosition.Y + Content_Open_Holder.AbsoluteSize.Y
 
 							-- Проверяем клик по кнопке Colorpicker
 							local onButton = Mouse.X >= Content_Holder.AbsolutePosition.X and 
