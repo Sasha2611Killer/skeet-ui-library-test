2d737d6f6b44b6fe5f7492c3d4a0f9e7ccea1e8f
Sasha2611Killer <158747806+Sasha2611Killer@users.noreply.github.com>
2026-07-03 20:19:19 +0800

    Update maybehavebugimnotcheck.lua

--- maybehavebugimnotcheck.lua
+++ maybehavebugimnotcheck.lua
@@ -1354,7 +1354,7 @@ end
 							BorderSizePixel = 0,
 							Parent = Content.Section.Extra,
 							Position = UDim2.new(0, Content_Holder_Outline.AbsolutePosition.X - Content.Section.Extra.AbsolutePosition.X, 0, Content_Holder_Outline.AbsolutePosition.Y - Content.Section.Extra.AbsolutePosition.Y + 21),
-							Size = UDim2.new(1, -98, 0, math.min((18 * #Content.Options) + 2, 150)),
+							Size = UDim2.new(1, -98, 0, math.min((18 * #Content.Options) + 2, 250)),
 							ZIndex = 6
 						})
 						-- //
